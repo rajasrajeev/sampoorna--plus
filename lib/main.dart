d14ed99e1ba7ba16eb3d5625db8c4bd675aa9597
viper1430 <viper1430@gmail.com>
2023-04-02 11:21:58 +0530

    changed title to Sampoorna

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sampoorna',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch().copyWith(
