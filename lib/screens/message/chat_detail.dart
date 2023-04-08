a397f931c23e80caebddf89399875b06398d3180
viper1430 <viper1430@gmail.com>
2023-04-08 16:25:15 +0530

    Update chat_detail.dart
    Message alignment sender and receiver update

--- lib/screens/message/chat_detail.dart
+++ lib/screens/message/chat_detail.dart
@@ -5,76 +5,126 @@ import 'package:student_management/constants.dart';
 
 class chatDetail extends StatefulWidget {
   final String? studentCode;
-  const chatDetail({required this.studentCode,super.key});
+  const chatDetail({required this.studentCode, super.key});
 
   @override
   State<chatDetail> createState() => _chatDetailState();
 }
 
+//Model class for Chat
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
+}
+
 class _chatDetailState extends State<chatDetail> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello,", messageType: "receiver"),
+    ChatMessage(messageContent: "Have you recieved Marklist?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey , I havent recieved",
+        messageType: "sender"),
+    ChatMessage(messageContent: "Marklist is updated ", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "k Thank you will check now", messageType: "sender"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          flexibleSpace: SafeArea(
-            child: Container(
-              padding: EdgeInsets.only(right: 16),
-              child: Row(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color:primaryColor,
-                    ),
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(right: 16),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: primaryColor,
                   ),
-                  SizedBox(
-                    width: 2,
+                ),
+                SizedBox(
+                  width: 2,
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/boy.png"),
+                  maxRadius: 20,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        widget.studentCode.toString(),
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
+                    ],
                   ),
-                  CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/boy.png"),
-                    maxRadius: 20,
-                  ),
-                  SizedBox(
-                    width: 12,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          widget.studentCode.toString(),
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 6,
-                        ),
-                        Text(
-                          "Online",
-                          style: TextStyle(
-                              color: Colors.grey.shade600, fontSize: 13),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
-        body: Stack(
+      ),
+      body: Stack(
         children: <Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                  padding:
+                      EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+                  child: Align(
+                      alignment: (messages[index].messageType == "receiver"
+                          ? Alignment.topLeft
+                          : Alignment.topRight),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: (messages[index].messageType == "receiver"
+                              ? Colors.grey.shade200
+                              : primaryColor),
+                        ),
+                        padding: EdgeInsets.only(
+                            left: 16, right: 16, top: 10, bottom: 10),
+                        child: Text(messages[index].messageContent,
+                        style: TextStyle(
+                          color: (messages[index].messageType == "receiver"
+                              ? Color.fromARGB(255, 0, 0, 0)
+                              : Color.fromARGB(255, 255, 255, 255)
+                              ),
+                        ),
+                        ),
+                      )));
+            },
+          ),
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
-              padding: EdgeInsets.only(left: 10,bottom: 10,top: 10),
+              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
               height: 60,
               width: double.infinity,
               color: Colors.white,
@@ -93,30 +143,36 @@ class _chatDetailState extends State<chatDetail> {
                   //     child: Icon(Icons.add, color: Colors.white, size: 20, ),
                   //   ),
                   // ),
-                  SizedBox(width: 15,),
+                  SizedBox(
+                    width: 15,
+                  ),
                   Expanded(
                     child: TextField(
                       decoration: InputDecoration(
-                        hintText: "Write message...",
-                        hintStyle: TextStyle(color: Colors.black54),
-                        border: InputBorder.none
-                      ),
+                          hintText: "Write message...",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none),
                     ),
                   ),
-                  SizedBox(width: 15,),
+                  SizedBox(
+                    width: 15,
+                  ),
                   FloatingActionButton(
-                    onPressed: (){},
-                    child: Icon(Icons.send,color: Colors.white,size: 18,),
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
                     backgroundColor: primaryColor,
                     elevation: 0,
                   ),
                 ],
-                
               ),
             ),
           ),
         ],
       ),
-        );
+    );
   }
 }
