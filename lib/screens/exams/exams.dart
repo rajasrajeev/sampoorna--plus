b9b38a20db9d30e75f37f4ba5dd3f2efeee1c00c
viper1430 <viper1430@gmail.com>
2023-04-25 20:49:36 +0530

    exams webview added

--- lib/screens/exams/exams.dart
+++ lib/screens/exams/exams.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:student_management/screens/parents/dashboard/parents_dashboard_screen.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:io';
+
+import '../../services/api_services.dart';
+import '../../services/jwt_token_parser.dart';
 
 class ExamsScreen extends StatefulWidget {
   const ExamsScreen({Key? key}) : super(key: key);
@@ -12,18 +18,65 @@ class ExamsScreen extends StatefulWidget {
 }
 
 class _ExamsScreenState extends State<ExamsScreen> {
-  bool isLoading = true;
+  bool _loading = true;
   String? token;
+  dynamic url;
+dynamic link;
 
   @override
   void initState() {
     super.initState();
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
+    getToken();
   }
 
   getToken() async {
     final prefs = await SharedPreferences.getInstance();
     token = prefs.getString('token');
+    final res = await getWebViewURL();
+    if (res.statusCode == 200) {
+      setState(() {
+        _loading = false;
+      });
+      final responseData = jsonDecode(res.body);
+
+      setState(() {
+         url = responseData['url'];
+      });
+      debugPrint("*******URL responseData**********");
+      debugPrint("$url");
+      debugPrint("*******responseData End**********");
+      final res2 = await getWebView(url);
+    debugPrint("*******response2 Data status code**********");
+    debugPrint(res2.statusCode.toString());
+    debugPrint("*******end response2 Data status code**********");
+    if (res2.statusCode == 200) {
+       setState(() {
+        _loading = false;
+      });
+     final response2Data = jsonDecode(res2.body);
+
+    setState(() {
+        link = response2Data['link'];
+      });
+           debugPrint("******* response2 Data Link **********");
+    debugPrint(Uri.decodeComponent(link).toString());
+    debugPrint("******* end response2 Data Link **********");
+    }
+    } else {
+      setState(() {
+        _loading = false;
+      });
+
+      Fluttertoast.showToast(
+        msg: "Unable to Load Now",
+        gravity: ToastGravity.TOP,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 15.0,
+      );
+    }
   }
 
   @override
@@ -59,18 +112,20 @@ class _ExamsScreenState extends State<ExamsScreen> {
                     WebView(
                       javascriptMode: JavascriptMode.unrestricted,
                       onWebViewCreated: (WebViewController webViewController) {
-                        var headers = {"Authorization": "Bearer $token"};
+                       var headers = {"Authorization": "Bearer $token"};
+                      var decodedlink = Uri.decodeComponent(link);
                         webViewController.loadUrl(
-                            'https://sampoorna.kite.kerala.gov.in:446/plus/loginApp',
-                            headers: headers);
+                          decodedlink,
+                            headers: headers
+                            );
                       },
                       onPageFinished: (finish) {
                         setState(() {
-                          isLoading = false;
+                          _loading = false;
                         });
                       },
                     ),
-                    isLoading
+                    _loading
                         ? const Center(child: CircularProgressIndicator())
                         : Stack(),
                   ],
